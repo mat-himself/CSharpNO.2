86b8e97e0d6faea71eb72e48978de9f5c3fa12a0
mat-himself <116955237+mat-himself@users.noreply.github.com>
2022-11-17 10:19:29 +0330

    Add files via upload
    Tamrin2 Taklif2

---
+++ Tamrin2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class test{ 
+
+
+public static void Main(){
+    string x=Console.ReadLine();
+int a=Convert.ToInt32(x);
+calculator(a);
+
+}
+private static void calculator(int z)
+{ 
+    int years,months,weeks;
+    years=z/365;
+    z=z%365;
+
+    months=z/30;
+    z=z%30;
+
+    weeks=z/7;
+    z=z%7;
+
+    Console.WriteLine(years);
+    Console.WriteLine(months);
+    Console.WriteLine(weeks);
+    Console.WriteLine(z);
+}
+}

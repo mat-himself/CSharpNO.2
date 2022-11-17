0328f21441600477a77033694e7be12998ca0ba2
mat-himself <116955237+mat-himself@users.noreply.github.com>
2022-11-17 09:57:50 +0330

    Add files via upload
    Tarmrin 1 Taklif 2

---
+++ Tamrin1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class test {
+    public static void Main(){
+        string a=Console.ReadLine();
+        Double x=Convert.ToDouble(a);
+        Console.WriteLine(Power(x));
+         
+
+    }
+    private static Double Power(Double b){ 
+
+    
+    Double y=Math.Pow(b,2);
+    Double z=Math.Pow(b,3);
+    return y;
+    return z;
+    }
+}
